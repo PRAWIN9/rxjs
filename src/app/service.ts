c19630e540c8d4c43bdfdf5d5de67fc2e33a5dc5
Praveen G <41817657+PRAWIN9@users.noreply.github.com>
2021-04-21 09:04:07 +0530

    Update service.ts

--- src/app/service.ts
+++ src/app/service.ts
@@ -1,5 +1,15 @@
 import { Injectable } from '@angular/core';
 @Injectable()
 export class Appservice{
-  
+  sub = Subject<any>
+  obj= Observable<number>
+
+  constructor(){
+    this.sub= new Subject<number>()
+    this.obj= this.sub.asObservable();
+  }
+  sendData(data:number){
+    this.sub.next(data);
+
+  }
 }
