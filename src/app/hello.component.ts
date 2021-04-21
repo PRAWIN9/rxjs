eac652868eeb33ce8fbab96c62040c33535d0a54
PRAWIN9 <prav16321.ec@rmkec.ac.in>
2021-04-21 10:26:14 +0530

    1 file modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -16,6 +16,7 @@ import { Appservice } from "./app.service";
 })
 export class HelloComponent {
   @Input() name: string;
+  isData= false;
   constructor(private ser: Appservice) {
     this.ser.sub.subscribe(x => {
       console.log("from hello sub ", x);
@@ -31,5 +32,12 @@ export class HelloComponent {
   emit() {
     this.ser.sendData(Math.random());
     this.ser.sendBehData(Math.random());
+
+    if(!this.isData){
+      this.isData=true;
+      this.ser.behobj.subscribe(x => {
+      console.log("from app beh obs ", x);
+    });
+    }
   }
 }
