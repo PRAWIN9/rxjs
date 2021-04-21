fbf204f7b8ab8f1a05a89b638fee2e6a4742df86
PRAWIN9 <prav16321.ec@rmkec.ac.in>
2021-04-21 09:32:41 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
+import { Appservice } from "./app.service";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+  fromsub = null;
+  constructor(private ser: Appservice) {
+    this.ser.sub.subscribe(x => {
+      console.log("from app sub ", x);
+      this.fromsub = x;
+    });
+  }
 }
