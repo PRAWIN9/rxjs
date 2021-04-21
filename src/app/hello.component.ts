2adc50efc346b090a4107f83ba5deaf4492f3637
PRAWIN9 <prav16321.ec@rmkec.ac.in>
2021-04-21 14:00:57 +0530

    1 file modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from "@angular/core";
-import { interval } from "rxjs";
+
 import { Appservice } from "./app.service";
 
 @Component({
@@ -28,10 +28,11 @@ export class HelloComponent {
       console.log("from hello sub obj ", x);
     });
 
-    // this.ser.behobj.subscribe(x => {
-    //   console.log("from hello beh obs ", x);
+    this.ser.behobj.subscribe(x => {
+      console.log("from hello beh obs ", x);
     
-    // });
+    });
+
 
 
   }
@@ -42,12 +43,12 @@ export class HelloComponent {
     this.ser.sendBehData(Math.random());
     this.ser.sendRepData(Math.random());
 
-    if(!this.isData){
-      this.isData=true;
-      this.ser.behobj.subscribe(x => {
-      console.log("from hello beh obj ", x);  //it will retain the value from hello obj
-    });
-    }
+    // if(!this.isData){
+    //   this.isData=true;
+    //   this.ser.behobj.subscribe(x => {
+    //   console.log("from hello beh obj ", x);  //it will retain the value from hello obj
+    // });
+    // }
     this.count++;
     if(this.count==3){
       this.ser.repobj.subscribe(x => {
