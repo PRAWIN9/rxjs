c0add8f3b84a50b7a9530af67b1f2fdd5fdb62e1
PRAWIN9 <prav16321.ec@rmkec.ac.in>
2021-04-21 09:30:59 +0530

    1 file modified

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,16 +1,15 @@
-import { Injectable } from '@angular/core';
-import { Subject, Observable} from 'rxjs'
+import { Injectable } from "@angular/core";
+import { Subject, Observable } from "rxjs";
 @Injectable()
-export class Appservice{
-  sub : Subject<number>
-  obj: Observable<number>
+export class Appservice {
+  sub: Subject<number>;
+  obj: Observable<number>;
 
-  constructor(){
-    this.sub= new Subject<number>()
-    this.obj= this.sub.asObservable();
+  constructor() {
+    this.sub = new Subject<number>();
+    this.obj = this.sub.asObservable();
   }
-  sendData(data:number){
+  sendData(data: number) {
     this.sub.next(data);
-
   }
 }
